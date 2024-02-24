f7f34cfbb5f4829c05cb603880d021df0e1e544d
zlwu <zilong.wu@gmail.com>
2024-02-25 00:37:45 +0800

    complete

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import PerspectionDecoration from './components/PerspectiveDecoration/PerspectionDecoration';
 
 function App() {
   return (
     <>
+      <PerspectionDecoration />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/PerspectiveDecoration/PerspectionDecoration.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './PerspectionDecoration.module.css';
+
+const PerspectionDecoration = () => {
+  return (
+    <div
+      className={styles.perspectionDecoration}
+    >
+    </div>
+  );
+};
+
+export default PerspectionDecoration;
